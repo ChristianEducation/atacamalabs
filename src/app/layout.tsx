11b916d89faf4eef23cf66e3cca761b7f361be78
ChristianEducation <c.wevarh@gmail.com>
2026-09-21 14:20:16 -0300

    style: Lety/IAutomatiza-inspired refinement layer (neutrals, pill buttons, glass header, slow reveal, hero grid+glow)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import "../styles/ui.css";
 import "../styles/demos.css";
 import "../styles/pages.css";
 import "../styles/inner.css";
+import "../styles/lety.css";
 
 /**
  * Fuentes autoalojadas (spec C2): Newsreader (títulos, 500 roman; cursiva 500
